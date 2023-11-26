e8dbe3d971649980cd50f2fe40f9ca32500c20f2
AdrianOjedaArenas <adrianojeda1997@hotmail.es>
2023-11-26 19:13:07 +0100

    TFG version recuperacion 1, navegacion detalles evento completado falta hacer la pantalla

--- app/src/main/java/com/example/tfg4/presentation/EventDetails/EventDetailsScreen.kt
+++ app/src/main/java/com/example/tfg4/presentation/EventDetails/EventDetailsScreen.kt
@@ -1,71 +1,102 @@
 package com.example.tfg4.presentation.EventDetails
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import com.example.tfg4.Database.Eventos
 import com.example.tfg4.Utilities.SharedViewModelEvento
-import com.example.tfg4.presentation.home.MainViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 @Composable
 fun DetailsScreen(
     navController: NavHostController,sharedViewModel: SharedViewModelEvento
 ) {
 
-   val evento = sharedViewModel.evento.value
+    val evento = sharedViewModel.evento.value
 
-   Column() {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
 
-       if (evento != null) {
-           //Titulo
-           evento.titulo?.let {
-               Text(
-                   text = it,
-                   modifier = Modifier.weight(1f),
-                   textAlign = TextAlign.Center,
-                   style = TextStyle(fontWeight = FontWeight.Bold)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Mostrar la foto en grande
 
-               )
-           }
+        Spacer(modifier = Modifier.size(16.dp))
 
-           Spacer(modifier = Modifier.size(20.dp))
+        // Título, fecha y hora
+        evento?.let {
+            Text(
+                text = it.titulo ?: "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                textAlign = TextAlign.Center,
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+            )
 
-           evento.descripcion?.let {
-               Text(
-                   text = it,
-                   modifier = Modifier.weight(1f),
-                   textAlign = TextAlign.Center,
-                   style = TextStyle(fontWeight = FontWeight.Bold)
+            val formattedDate = it.fecha?.let { it1 -> dateFormat.format(it1) }// formateo fecha dd/mm/yyyy
 
-               )
-           }
+            Text(
+                text = "Fecha: ${formattedDate}   Hora: ${it.hora}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                textAlign = TextAlign.Center,
+                style = TextStyle(fontSize = 16.sp)
+            )
 
-           Spacer(modifier = Modifier.size(20.dp))
+            Spacer(modifier = Modifier.size(16.dp))
 
-       }
+            // Descripción
+            Text(
+                text = it.descripcion ?: "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                textAlign = TextAlign.Center,
+                style = TextStyle(fontSize = 18.sp)
+            )
 
+            Spacer(modifier = Modifier.size(16.dp))
 
+            // Botones para unirse o quitarse del evento
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = { /* Lógica para unirse al evento */ },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(4.dp)
+                ) {
+                    Text(text = "Unirse")
+                }
 
-
-    }//Termina columna
-
-
-}
-
-fun detaisl(eventos: Eventos){
-
-
-
+                Button(
+                    onClick = { /* Lógica para quitarse del evento */ },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(4.dp)
+                ) {
+                    Text(text = "Quitarse")
+                }
+            }
+        }
+    }
 }
 
 
